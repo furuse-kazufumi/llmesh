e8051c2cc73608b04da82705f13554b81cd6c3b3
furuse-kazufumi <kazufumi@furuse.work>
2026-05-12 07:35:30 +0900

    auto: __init__.py 編集前 (2026-05-12 07:35)

--- llmesh/vla/__init__.py
+++ llmesh/vla/__init__.py
@@ -83,6 +83,8 @@ __all__ = [
     "ActionStream",
     "BCEvalReport",
     "BCPolicy",
+    "ConstraintFn",
+    "ConstraintViolation",
     "EpisodeOutcome",
     "EvaluationReport",
     "ExecutionFault",
